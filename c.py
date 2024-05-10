62702bd27e005fe7a23e2f8fe163dd26f2f02168
Ivan <34632454+abreulima@users.noreply.github.com>
2024-05-10 16:58:22 +0000

    Add files via upload

--- c.py
+++ c.py
@@ -14,7 +14,7 @@ BUFFERSIZE = 2048
 
 playerid = 0
 
-sprite1 = pygame.image.load('sprites\Imagem-1.png')
+sprite1 = pygame.image.load('sprites/Imagem-1.png')
 
 
 class Game:
@@ -64,27 +64,76 @@ async def send_message(message):
     # Connect to the server
     reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
 
+    game_event = await reader.read(1024)
+    game_event_response = pickle.loads(game_event)
+    print(f"Received response from server: {game_event_response}")
+
+    if game_event_response[0] == "id_update":
+        client_player.id = game_event_response[1]
+        print("Player ID", client_player.id)
+
+
 
     while True:
 
         #user_input = input("Enter Movement")
-        serialized_message = pickle.dumps("a")
+        ##serialized_message = pickle.dumps("a")
+        ##writer.write(serialized_message)
+        ##await writer.drain()
 
-        writer.write(serialized_message)
-        await writer.drain()
-
-        """
+        
         # Read the response from the server
-        response = await reader.read(1024)
-        response_message = pickle.loads(response)
-        print(f"Received response from server: {response_message}")
-        """
+        #response = await reader.read(1024)
+        #response_message = pickle.loads(response)
+        #print(f"Received response from server: {response_message}")
+        
+        #done, pending = await asyncio.wait([reader], timeout=0)
+
+        #for task in done:
+        #    data = await task.result().read(1024)
+        #    print(pickle.loads(data))
+
+        #if game_event[0] == "id_update":
+        #    pass
+            #Player.playerid = game_event[1]
+            #print(playerid)
+
 
         for event in pygame.event.get():
             
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+
+            if event.type == pygame.KEYDOWN:
+            
+                if event.key == pygame.K_LEFT:
+                    client_player.vx = -10
+
+                if event.key == pygame.K_RIGHT:
+                    client_player.vx = 10
+
+                if event.key == pygame.K_UP:
+                    client_player.vy = -10
+
+                if event.key == pygame.K_DOWN:
+                    client_player.vy = 10 
+
+            if event.type == pygame.KEYUP:
+
+                if event.key == pygame.K_LEFT and client_player.vx == -10:
+                    client_player.vx = 0    
+
+                if event.key == pygame.K_RIGHT and client_player.vx == 10:
+                    client_player.vx = 0
+
+                if event.key == pygame.K_UP and client_player.vy == -10:
+                    client_player.vy = 0   
+
+                if event.key == pygame.K_DOWN and client_player.vy == 10:
+                    client_player.vy = 0   
+
 
         game.clock.tick(60)
         game.screen.fill((255, 255, 255))
@@ -97,6 +146,19 @@ async def send_message(message):
         client_player.render()
 
         pygame.display.flip()
+
+
+        updateLocation = [
+            "position_update",
+            client_player.id,
+            client_player.x,
+            client_player.y
+        ]
+
+
+        writer.write(pickle.dumps(updateLocation))
+        await writer.drain()
+
 
     writer.close()
     await writer.wait_closed()
--- s.py
+++ s.py
@@ -1,11 +1,33 @@
 import asyncio
-import pickle 
+import pickle
+import random
+
 
 ADDR = '127.0.0.1'
 PORT = 8888
 
+connections = []
+players_map = {}
+
+class Player:
+    def __init__(self, owner_id):
+        self.owner_id = owner_id
+        self.x = 0
+        self.y = 0
+
+
 async def handle_client(reader, writer):
+
+    connections.append(writer)
     
+    player_id = random.randint(0, 100000)
+    main_player = Player(player_id)
+    players_map[player_id] = main_player
+
+    writer.write(pickle.dumps(["id_update", player_id]))
+    await writer.drain()
+
+
     while True:
 
         data = await reader.read(1024)
@@ -14,10 +36,44 @@ async def handle_client(reader, writer):
             break
 
         message = pickle.loads(data)
-        print(f"Received {message}")
+        #print(f"Received {message}")
+        
 
-        writer.write(data)
-        await writer.drain()
+        if message[0] == "position_update":
+
+            player_id = message[1]
+            x = message[2]
+            y = message[3]
+
+            print(message[1], message[2], message[3])
+
+            if player_id == 0:
+                return
+            
+            players_map[player_id].x = x
+            players_map[player_id].y = y
+
+
+        remove = []
+        
+        for conn in connections:
+
+            update = ["player_locations"]
+
+            for key, value in players_map.items():
+                update.append([value.owner_id, value.x, value.y])
+
+            try:
+                conn.write(pickle.dumps(update))
+                await conn.drain()
+            except asyncio.CancelledError:
+                remove.append(conn)
+            except Exception as e:
+                print(f"Error sending data to client: {e}")
+                remove.append(conn)
+
+            for r in remove:
+                connections.remove(r)
 
     print("Client disconnected")
     writer.close()
